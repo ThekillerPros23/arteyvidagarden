0dc3ca52b502fb96d54434ba25c35b28f56a1132
Roberto Goddard <mgroberto38@hotmail.com>
2025-01-18 02:35:14 -0500

    gasgasg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Services from "./components/Services";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
       </Routes>
     </BrowserRouter>
   );
